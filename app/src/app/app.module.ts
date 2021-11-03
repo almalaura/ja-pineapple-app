c3be7cb7a8781855cdb5140b90300ce8ee65318d
almalaura <50027180+almalaura@users.noreply.github.com>
2021-11-03 12:42:29 -0600

    components

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProductsComponent,
+    HomeComponent,
+    UsersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
