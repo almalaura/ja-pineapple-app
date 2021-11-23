4871be416abd7ed887f3ee95e407c8869d84d64a
MBlev <loudev07@gmail.com>
2021-11-23 17:01:45 -0500

    Reset password

--- app/src/app/components/login/forgot-password.component.ts
+++ app/src/app/components/login/forgot-password.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/service/authentication.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    protected fb: FormBuilder) { }
+
+
+  form: FormGroup
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])]
+    })
+  }
+
+  async sendEmail(): Promise<void> {
+    if (this.form.valid) {
+      await this.authService.checkEmail(this.form.get('email')?.value).toPromise().then(
+        (user) => {
+          this.router.navigate(['/reset-password'], {
+            queryParams: {
+              id: user.id
+            },
+            queryParamsHandling: 'merge',
+          })
+        }
+      ).catch((_) => {
+        Swal.fire('Correo no encontrado', 'El correo introducido no existe por favor ingresar el correo con el que se registró')
+      })
+    }
   }
 
 }
--- app/src/app/components/login/reset-password.component.ts
+++ app/src/app/components/login/reset-password.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, mergeMap, tap } from 'rxjs/operators';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/service/authentication.service';
+import Swal from 'sweetalert2';
+import { UserService } from '../users/user.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +15,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  id$: Observable<number>
+  
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthenticationService,
+    protected fb: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      password: ['', Validators.compose([
+        Validators.required
+      ])],
+      confirmPassword: ['', Validators.compose([
+        Validators.required
+      ])]
+    }, { validators: this.checkPasswords })
+    this.id$ = this.route.queryParams.pipe(
+      map(params => params.id)
+    )
+  }
+
+  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
+    let pass = group.get('password')?.value;
+    let confirmPass = group.get('confirmPassword')?.value
+    return pass === confirmPass ? null : { notSame: true }
+  }
+
+  resetPassword(id: number): void {
+    if (this.form.valid) {
+      this.authService.resetPassword(id, this.form.get('confirmPassword')?.value).subscribe(
+        (_) => {
+          Swal.fire('Contraseña restablecidad con exito')
+          this.router.navigate(['/login'])
+        }
+      )
+    }
   }
 
 }
